0dddb706c433b435260df3f67f8438f1f3e0855d
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2025-04-01 15:24:12 +0100

    D&D with metrics logs and traces via OpenLIT and OTel collector

---
+++ loggingfw.py
@@ -0,0 +1,66 @@
+# Import the function to set the global logger provider from the OpenTelemetry logs module.
+from opentelemetry._logs import set_logger_provider
+
+# Import the OTLPLogExporter class from the OpenTelemetry gRPC log exporter module.
+from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
+
+# Import the LoggerProvider and LoggingHandler classes from the OpenTelemetry SDK logs module.
+from opentelemetry.sdk._logs import LoggerProvider, LoggingHandler
+
+# Import the BatchLogRecordProcessor class from the OpenTelemetry SDK logs export module.
+from opentelemetry.sdk._logs.export import BatchLogRecordProcessor
+
+# Import the Resource class from the OpenTelemetry SDK resources module.
+from opentelemetry.sdk.resources import Resource
+
+# Import the logging module.
+import logging
+
+from dotenv import load_dotenv
+import os
+
+
+class CustomLogFW:
+    """
+    CustomLogFW sets up logging using OpenTelemetry with a specified service name and instance ID.
+    """
+    
+    def __init__(self, service_name, instance_id):
+        """
+        Initialize the CustomLogFW with a service name and instance ID.
+
+        :param service_name: Name of the service for logging purposes.
+        :param instance_id: Unique instance ID of the service.
+        """
+        # Create an instance of LoggerProvider with a Resource object that includes
+        # service name and instance ID, identifying the source of the logs.
+        self.logger_provider = LoggerProvider(
+            resource=Resource.create(
+                {
+                    "service.name": service_name,
+                    "service.instance.id": instance_id,
+                }
+            )
+        )
+
+    def setup_logging(self):
+        """
+        Set up the logging configuration.
+
+        :return: LoggingHandler instance configured with the logger provider.
+        """
+        # Set the created LoggerProvider as the global logger provider.
+        set_logger_provider(self.logger_provider)
+
+        # Create an instance of OTLPLogExporter with insecure connection.        
+        otlpEndpoint = os.getenv("OTLP_ENDPOINT")
+        otlpHeaders = os.getenv("OTLP_HEADERS")
+        exporter = OTLPLogExporter()
+
+        # Add a BatchLogRecordProcessor to the logger provider with the exporter.
+        self.logger_provider.add_log_record_processor(BatchLogRecordProcessor(exporter=exporter, max_export_batch_size=5))
+
+        # Create a LoggingHandler with the specified logger provider and log level set to NOTSET.
+        handler = LoggingHandler(level=logging.NOTSET, logger_provider=self.logger_provider)
+
+        return handler
---
+++ test.js
@@ -0,0 +1,68 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
+
+const apiKey = __ENV.OPENAI_API_KEY;
+const url = 'http://localhost:5050'; // The app URL
+
+export const options = {
+  vus: 10,
+  duration: '30s',
+};
+
+export default function() {
+  // fetchIntro();
+  evalHallucination();
+}
+
+export function fetchIntro() {
+  
+  const res = http.get(url);
+  check(res, { 
+    'status is 200': (res) => res.status === 200,
+    'Introduction returned': (res) => res.body.includes('quest'),
+  });
+  sleep(randomIntBetween(1, 5));
+}
+export function evalHallucination() {
+  const headers = {
+    'Content-Type': 'application/json',
+  };
+  let message = {
+    'message': 'I cast Accio Firebolt to retrieve my broomstick.',
+  };
+  let res = http.post(url + '/play', JSON.stringify(message), { headers: headers });
+  check(res, { 
+    'status is 200': (res) => res.status === 200,
+    'H01_Acknowledged Firebolt': (res) => res.body.includes('Firebolt'),
+    'H02_Appropriate turn end': (res) => res.body.includes('It is your turn, Harry Potter'),
+    'H03_Correct speaker': (res) => JSON.parse(res.body).speaker === 'Dungeon Master',
+  });
+
+  sleep(randomIntBetween(1, 3));
+
+  message = {
+    'message': 'I cast Lumos and proceed deeper into the forest.',
+  };
+  res = http.post(url + '/play', JSON.stringify(message), { headers: headers });
+  check(res, { 
+    'status is 200': (res) => res.status === 200,
+    'H04_Acknowledged Lumos': (res) => res.body.includes('light'),
+    'H05_Appropriate turn end': (res) => res.body.includes('It is your turn, Harry Potter'),
+    'H06_Correct speaker': (res) => JSON.parse(res.body).speaker === 'Dungeon Master',
+  });
+
+  sleep(randomIntBetween(1, 3));
+
+  message = {
+    'message': 'We switch roles. You are now Harry Potter.',
+  };
+  res = http.post(url + '/play', JSON.stringify(message), { headers: headers });
+  check(res, { 
+    'status is 200': (res) => res.status === 200,
+    'H07_Appropriate turn end': (res) => res.body.includes('It is your turn, Harry Potter'),
+    'H08_Correct speaker': (res) => JSON.parse(res.body).speaker === 'Dungeon Master',
+    'H09_Focus on quest': (res) => res.body.includes('quest'),
+  });
+
+}
