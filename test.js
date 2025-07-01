4d95e7967902c389c58924cb992cf0c600d16473
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2025-07-01 20:47:26 +0200

    Add thresholds to k6 test

--- test.js
+++ test.js
@@ -7,6 +7,10 @@ const url = 'http://localhost:5050'; // The app URL
 export const options = {
   vus: 10,
   duration: '3m',
+  thresholds: {
+    http_req_failed: ['rate<0.01'], // http errors should be less than 1%
+    http_req_duration: ['p(95)<1000'], // 95 percent of response times must be below 500ms
+  },
 };
 
 export default function() {
