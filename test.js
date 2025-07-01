849f6f36a811a12e479da422ecb81efecf6a3758
Nicole van der Hoeven <nicole@nicolevanderhoeven.com>
2025-07-01 20:29:16 +0200

    Improve k6 test

--- test.js
+++ test.js
@@ -2,7 +2,6 @@ import http from 'k6/http';
 import { sleep, check } from 'k6';
 import { randomIntBetween } from 'https://jslib.k6.io/k6-utils/1.2.0/index.js';
 
-const apiKey = __ENV.OPENAI_API_KEY;
 const url = 'http://localhost:5050'; // The app URL
 
 export const options = {
@@ -17,7 +16,7 @@ export default function() {
 
 export function fetchIntro() {
   
-  console.log('Loki logs sent from k6');
+  lokiClient.push('Loki logs sent from k6');
   const res = http.get(url);
   let success = check(res, { 
     'status is 200': (res) => res.status === 200,
